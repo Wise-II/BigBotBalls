7cf6c1f8e2c8a11b6af423b3bb5292d9c53435da
Diego <81892372+Wise-II@users.noreply.github.com>
2023-12-13 13:29:00 -0500

    1 day in
    started writing the command handler function

--- Index.js
+++ Index.js
@@ -4,4 +4,13 @@ const fs = require('fs');
 const path = require('path');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILDS_MESSAGES]});
+// prefix config
 const config = require('./config.json');
+const prefix = config.prefix
+
+client.commands = new Map();
+
+// Function to execute commands from files
+const loadCommands = (dir) => {
+    const commandFiles = fs.readdirSync(path.join(__dirname, dir)).filter
+}
